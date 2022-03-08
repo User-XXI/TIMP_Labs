e6c765d9bdf960477a668def9d53fec2f5139514
User-XXI <andmak96@gmail.com>
2022-03-08 15:08:24 +0300

    ""

--- sources/hello_world.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main(int argc, char** argv){string name;cout << "Input user_name";cin >> name;cout << "Hello world from " << name << endl; return 0;}
-
